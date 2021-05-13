2787d8eb5f3a0a014ae27f0ede83a51dde5acee4
Christopher Harrison <chrhar@microsoft.com>
2021-05-13 10:58:24 -0700

    Updated for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		files: {
+			assets: 'static'
+		}
 	}
 };
-
-export default config;
